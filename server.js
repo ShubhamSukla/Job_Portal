47f039d65d5fb307f5112d24d80189038e45c885
Shubham Shukla <shuklashub342003@gmail.com>
2024-04-04 21:33:33 +0530

    Server create

---
+++ server.js
@@ -0,0 +1,20 @@
+
+//const express=require('express');
+import  express  from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+dotenv.config()
+
+const app=express()
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to job portal</h1>")
+})
+
+const PORT=process.env.PORT||8080
+
+app.listen(PORT,()=>{
+    console.log(`Node server running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan.white);
+})
