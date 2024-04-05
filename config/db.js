a3c47e46e4ac158d0fdc85233d934e4ddfaa8198
Shubham Shukla <shuklashub342003@gmail.com>
2024-04-05 23:40:41 +0530

    Database Connected

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDb=async()=>{
+    try{
+        const conn =await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected To mongodb Database ${mongoose.connection.host}`.bgMagenta.white);
+    }catch(error)
+    {
+        console.log(`MongoDB Error ${error}`.bgRed.white)
+    }
+};
+
+export default connectDb;
--- server.js
+++ server.js
@@ -3,8 +3,14 @@
 import  express  from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import connectDb from './config/db.js';
+
 
 dotenv.config()
+
+// mongodb connnection
+
+connectDb();
 
 const app=express()
 
@@ -17,4 +23,4 @@ const PORT=process.env.PORT||8080
 
 app.listen(PORT,()=>{
     console.log(`Node server running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan.white);
-})
+});
