1ba06f3f63d37fe68ec19203020cbdaa4a516705
Shubham Shukla <shuklashub342003@gmail.com>
2024-04-20 19:37:15 +0530

    Job API

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createJobController, getAllJobsController } from "../controllers/jobsController.js";
+import { createJobController, getAllJobsController, updateJobController, deleteJobController } from "../controllers/jobsController.js";
 import userAuth from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,6 +7,15 @@ const router = express.Router();
 //routes
 // CREATE JOB || POST
 router.post("/create-job", userAuth, createJobController);
+
+//GET JOB
 router.get("/get-job", userAuth, getAllJobsController);
+
+//UPDATE JOBS ||  PATCH
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+//DELETE JOBS || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
+
 
 export default router;
